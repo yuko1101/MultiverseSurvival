abe87b829df95b51ec5383083a4df82a0ed40264
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-12-11 13:22:06 +0900

    add multiverse-core with some implementing code

--- src/main/kotlin/io/github/yuko1101/multiversesurvival/MultiverseSurvival.kt
+++ src/main/kotlin/io/github/yuko1101/multiversesurvival/MultiverseSurvival.kt
@@ -1,10 +1,22 @@
 package io.github.yuko1101.multiversesurvival
 
+import com.onarandombox.MultiverseCore.MultiverseCore
+import org.bukkit.Bukkit
+import org.bukkit.World
+import org.bukkit.WorldType
 import org.bukkit.plugin.java.JavaPlugin
 
-class MultiverseSurvival : JavaPlugin() {
-    override fun onEnable() {
 
+class MultiverseSurvival : JavaPlugin() {
+
+    companion object {
+        lateinit var core: MultiverseCore
+    }
+
+    override fun onEnable() {
+        core = Bukkit.getServer().pluginManager.getPlugin("Multiverse-Core") as MultiverseCore
+
+        core.mvWorldManager.addWorld("resource_world", World.Environment.NORMAL, null, WorldType.NORMAL, true, null)
     }
 
     override fun onDisable() {
