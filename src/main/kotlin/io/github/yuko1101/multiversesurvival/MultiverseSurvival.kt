a78838305065b6f0407a7d826387884624527fb7
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-12-11 12:53:30 +0900

    setup debug env and kotlin

---
+++ src/main/kotlin/io/github/yuko1101/multiversesurvival/MultiverseSurvival.kt
@@ -0,0 +1,13 @@
+package io.github.yuko1101.multiversesurvival
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class MultiverseSurvival : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
